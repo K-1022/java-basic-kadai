1e52ac17c065b449ffd6edf54fe1b085b863d83e
K-1022 <167005898+K-1022@users.noreply.github.com>
2024-06-25 03:14:58 +0900

    確認お願いします。

---
+++ kadai_024/JyankenExec_Chapter24.java
@@ -0,0 +1,12 @@
+package kadai_024;
+
+public class JyankenExec_Chapter24 {
+
+	public static void main(String[] args) {
+		Jyanken_Chapter24  jyanken = new Jyanken_Chapter24(); 
+		
+		jyanken.playGame();
+	
+	}
+
+}
